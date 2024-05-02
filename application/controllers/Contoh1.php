f5812901115ee33c412942aa3b4dc92c75b23801
deajhubneramnda12 <amnddea1507@gmail.com>
2024-05-02 13:04:14 +0700

    tugas pertemuan1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Dea Amanda
+            Saya tinggal di daerah Tanjung Raya II 
+            olah raga yang saya sukai adalah 
+            Bola Volly"; 
+    } 
+}
