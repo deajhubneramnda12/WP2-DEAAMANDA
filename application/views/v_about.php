fd233675374d42604545baae346fc66854aee0a1
deajhubneramnda12 <amnddea1507@gmail.com>
2024-05-06 07:58:17 +0700

    Tugas 3 pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dea </li>
+    </ul>
+    <ul type="disc">
+        <li>Nama Belakang : Amanda </li>
+    </ul>
+    
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Amanah</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kapuas Hulu</li>
+    </ul>
+    
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bola Volley</li>
+        <li>Nonton Drakor</li>
+    </ul>
+</section>
